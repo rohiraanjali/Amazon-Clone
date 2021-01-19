f6617cdea2fcf72ef2520627845d8f94c649a520
Anjali Rohira <rohiraanjali@gmail.com>
2021-01-19 23:42:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import "./styles.css";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      {/* Home */}
+    </div>
+  );
+}
